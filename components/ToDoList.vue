91b63d7e89dd5233cde13ebb6ff1c0304a1f5f10
walley <rajiolawale2020@gmai.com>
2021-03-01 23:22:31 +0100

    vueTodo

---
+++ components/ToDoList.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="to-do-container">
+    <div class="create-container">
+      <input v-model="titleOfNewToDo" type="text">
+      <button @click="create">
+        Add
+      </button>
+    </div>
+    <div class="to-do-list-container">
+  <to-do-list-item v-for="todo in todos" :key="todo.id" :todo="todo" v-on:delete="deleteItem" />
+</div>
+  </div>
+</template>
+
+<script>
+import ToDoListItem from '~/components/ToDoListItem.vue'
+export default {
+    components: {
+    ToDoListItem
+  },
+  props: {
+    todos: {
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  },
+  data () {
+    return {
+      titleOfNewToDo: ''
+    }
+  },
+  methods: {
+   create() {
+     this.todos.push({ id: this.titleOfNewToDo, title: this.titleOfNewToDo, completed: false })
+     this.titleOfNewToDo = ''
+   },
+   deleteItem (item) {
+      this.todos.splice(this.todos.indexOf(item), 1)
+    }
+}
+}
+</script>
+
+<style >
+.to-do-container {
+  width: 400px;
+  margin: auto;
+  margin-top: 50px;
+  display: block;
+}
+  .create-container {
+    width: 100%;
+    height: 50px;
+    box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.1);
+  }
+    input {
+      height: 50px;
+      width: 250px;
+      border: none;
+      padding-left: 10px;
+      font-family: "Quicksand", "Source Sans Pro", -apple-system,
+        BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial,
+        sans-serif;
+      font-size: 18px;
+      }
+    
+
+    button {
+      height: 45px;
+      float: right;
+      background: none;
+      width: auto;
+      margin: 0;
+      border: none;
+      cursor: pointer;
+      text-align: right;
+      padding-right: 20px;
+      font-size: 18px;
+    
+    
+  }
+
+.to-do-list-container {
+  width: 400px;
+  margin: auto;
+  margin-top: 50px;
+  display: block;
+  box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.1);
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="container">
+    <h1 class="title">
+      My To Do List
+    </h1>
+    <to-do-list :todos="todos" />
+  </div>
+</template>
+
+<script>
+import ToDoList from '~/components/ToDoList.vue';
+export default {
+   components: {
+    ToDoList
+  },
+  
+  data () {
+    return {
+      todos: {
+        type: Array
+      }
+    }
+  },
+  asyncData (ctx) {
+    return {
+      todos: [
+        {
+          'id': '1',
+          'title': 'A',
+          'completed': false
+        },
+        {
+          'id': '2',
+          'title': 'B',
+          'completed': true
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  display: flex;
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  flex-direction: column;
+}
+
+.title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
